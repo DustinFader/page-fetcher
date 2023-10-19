d4f86b36922bee408eafa9ce02dac9ceca9a6086
DustinFader <dustinfader16@gmail.com>
2023-10-19 10:33:44 -0300

    Im gonna stop playing around and save it for later

--- fetcher.js
+++ fetcher.js
@@ -1,29 +1,29 @@
 const request = require('request');
 const fs = require('fs');
-const readline = require('readline')
-const { stdin: input, stdout: output } = require('process');
+// const readline = require('readline')
+// const { stdin: input, stdout: output } = require('process');
 
 const URL = 'http://www.example.edu/';
-const filePath = 'files/index.txt';
-const rl = readline.createInterface({
-  input, output});
+const filePath = 'files/test.txt';
+// const rl = readline.createInterface({
+//   input, output});
 
 // checks if file exists
-async function fileExists (filePath) {
-  try {
-    fs.access(filePath)
-    return true
-  }
-  catch { return false }
-}
+// async function fileExists (filePath) {
+//   try {
+//     fs.access(filePath)
+//     return true
+//   }
+//   catch { return false }
+// }
 
 // request
 request(URL, (error, responce, body) => {
   if (error) console.log('error', error);
-  if (fileExists(filePath)) {
+  // if (fileExists(filePath)) {
     // if file exists then ask to overwrite.
-    rl.question('File already exists. Press Y to overwrite.\n', (answer) => {
-      if (answer.toUpperCase() === 'Y') {
+    // rl.question('File already exists. Press Y to overwrite.\n', (answer) => {
+      // if (answer.toUpperCase() === 'Y') {
         fs.writeFile(filePath, body, err => {
           if (err) {
             console.error(err);
@@ -31,8 +31,8 @@ request(URL, (error, responce, body) => {
           }
           console.log(`Downloaded and saved ${body.length} bytes to ${filePath}`)
         });
-      }
-      rl.close()
-    })
-  }
+      // }
+      // rl.close()
+    // })
+  // }
 });
