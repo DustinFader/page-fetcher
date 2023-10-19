87e842eb0f755a6a3974b4ae322c0396654af6df
DustinFader <dustinfader16@gmail.com>
2023-10-19 12:57:57 -0300

    started over with a fresh mind. Writes url page into new file or overwrites existing file.

--- fetcher.js
+++ fetcher.js
@@ -1,38 +1,19 @@
 const request = require('request');
 const fs = require('fs');
-// const readline = require('readline')
-// const { stdin: input, stdout: output } = require('process');
 
-const URL = 'http://www.example.edu/';
-const filePath = 'files/test.txt';
-// const rl = readline.createInterface({
-//   input, output});
+// node fetcher.js <website page> <file path>
+// example: node fetcher.js 'http://www.example.edu/' 'files/test.txt'
+let { [0]:URL, [1]:filePath } = process.argv.splice(2);
 
-// checks if file exists
-// async function fileExists (filePath) {
-//   try {
-//     fs.access(filePath)
-//     return true
-//   }
-//   catch { return false }
-// }
-
-// request
+// request for url page
 request(URL, (error, responce, body) => {
   if (error) console.log('error', error);
-  // if (fileExists(filePath)) {
-    // if file exists then ask to overwrite.
-    // rl.question('File already exists. Press Y to overwrite.\n', (answer) => {
-      // if (answer.toUpperCase() === 'Y') {
-        fs.writeFile(filePath, body, err => {
-          if (err) {
-            console.error(err);
-            return;
-          }
-          console.log(`Downloaded and saved ${body.length} bytes to ${filePath}`)
-        });
-      // }
-      // rl.close()
-    // })
-  // }
+  // write into file to givin path
+  fs.writeFile(filePath, body, err => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(`Downloaded and saved ${body.length} bytes to ${filePath}`)
+  });
 });
